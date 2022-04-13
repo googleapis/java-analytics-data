31c8dcced192d4d7e94a738d8337b57c369bdcb2
Anwesha <anweshanaskar@gmail.com>
2022-04-13 15:28:38 -0700

    Style changes on comment

--- samples/snippets/src/main/java/com/example/analytics/GetMetadataByPropertyIdSample.java
+++ samples/snippets/src/main/java/com/example/analytics/GetMetadataByPropertyIdSample.java
@@ -37,10 +37,7 @@ import com.google.analytics.data.v1beta.Metadata;
 public class GetMetadataByPropertyIdSample {
 
   public static void main(String... args) throws Exception {
-    /**
-     * TODO(developer): Replace this variable with your Google Analytics 4 property ID before
-     * running the sample.
-     */
+    // TODO(developer): Replace with your Google Analytics 4 property ID before running the sample.
     String propertyId = "YOUR-GA4-PROPERTY-ID";
     sampleGetMetadataByPropertyId(propertyId);
   }
