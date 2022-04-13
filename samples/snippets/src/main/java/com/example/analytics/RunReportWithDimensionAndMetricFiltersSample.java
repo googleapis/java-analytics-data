2393e6f033a71901b000322f2cbb8a82d3e754bb
Anwesha <anweshanaskar@gmail.com>
2022-04-13 15:14:03 -0700

    Style changes on comment

--- samples/snippets/src/main/java/com/example/analytics/RunReportWithDimensionAndMetricFiltersSample.java
+++ samples/snippets/src/main/java/com/example/analytics/RunReportWithDimensionAndMetricFiltersSample.java
@@ -48,10 +48,7 @@ import com.google.analytics.data.v1beta.RunReportResponse;
 public class RunReportWithDimensionAndMetricFiltersSample {
 
   public static void main(String... args) throws Exception {
-    /**
-     * TODO(developer): Replace this variable with your Google Analytics 4 property ID before
-     * running the sample.
-     */
+    // TODO(developer): Replace with your Google Analytics 4 property ID before running the sample.
     String propertyId = "YOUR-GA4-PROPERTY-ID";
     sampleRunReportWithDimensionAndMetricFilters(propertyId);
   }
